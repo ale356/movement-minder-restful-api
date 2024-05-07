9f116145f0e76c0971fb34b42116db489ec919e8
Alejandro <al225vh@student.lnu.se>
2024-05-07 15:43:44 +0800

    Add the model TimeTracker

---
+++ src/models/timeTracker.js
@@ -0,0 +1,49 @@
+/**
+ * Mongoose model TimeTracker.
+ *
+ * @author Mats Loock
+ * @version 2.0.0
+ */
+
+import mongoose from 'mongoose'
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference the User model
+    required: true
+  },
+  totalSedentaryTime: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  totalBreakTime: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true, // ensure virtual fields are serialized
+    /**
+     * Performs a transformation of the resulting object to remove sensitive information.
+     *
+     * @param {object} doc - The mongoose document which is being converted.
+     * @param {object} ret - The plain object representation which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret.__v
+      delete ret._id
+    }
+  }
+})
+
+schema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+// Create a model using the schema.
+export const Task = mongoose.model('Task', schema)
